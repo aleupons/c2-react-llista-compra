bb58d55488fbd7016965416a127072005d8d51c4
Pol Reig <polreigbroto@gmail.com>
2021-06-22 12:37:25 +0200

    Carpeta paginas con archivos en ROUTER creadas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import {
+  Redirect,
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
+import { AcercaDe } from "./paginas/AcercaDe";
+import { Formulario } from "./paginas/Formulario";
+import { Lista } from "./paginas/Lista";
+import { Principal } from "./paginas/Principal";
+
 function App() {
-  return <p>Hola</p>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/principal" exact>
+          <Principal />
+        </Route>
+        <Route path="/acerca-de" exact>
+          <AcercaDe />
+        </Route>
+        <Route path="/formulario" exact>
+          <Formulario />
+        </Route>
+        <Route path="/lista" exact>
+          <Lista />
+        </Route>
+        <Route path="/" exact>
+          <Redirect to="/principal" />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
